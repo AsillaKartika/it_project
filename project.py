fa6792c4954e90720b3fc239d4501d8775890a33
AsillaKartika <116348643+AsillaKartika@users.noreply.github.com>
2022-11-21 12:24:42 +0700

    update

--- project.py
+++ project.py
@@ -13,4 +13,4 @@ data_show= pd.DataFrame(data={'negatif' : [neg], 'positif': [pos], 'netral':[net
 st.title('Selamat datang di aplikasi Senasi')
 
 st.subheader('Hasil Sentimen Inflasi dari komentar twitter')
-st.pie_chart(data_show.T)
+st.pie_chart(data_show)
