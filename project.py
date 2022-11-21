613106ba123e4574e4fec39f8db81c11ae68b799
AsillaKartika <116348643+AsillaKartika@users.noreply.github.com>
2022-11-21 12:29:11 +0700

    update

--- project.py
+++ project.py
@@ -13,4 +13,4 @@ data_show= pd.DataFrame(data={'negatif' : [neg], 'positif': [pos], 'netral':[net
 st.title('Selamat datang di aplikasi Senasi')
 
 st.subheader('Hasil Sentimen Inflasi dari komentar twitter')
-st.bar_chart((data_show.T), color=['red', 'blue', 'magenta'])
+st.bar_chart(data_show.T) color=['red', 'blue', 'magenta']
