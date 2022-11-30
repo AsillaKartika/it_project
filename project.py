b9f6a12400bc8565d26e0b6bf939ce41d4e1c08d
AsillaKartika <116348643+AsillaKartika@users.noreply.github.com>
2022-11-30 15:00:38 +0700

    Add files via upload

--- project.py
+++ project.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
+import pickle  
+
+# load the model from disk
+loaded_model = pickle.load(open('tweets_all_data_clean_model.sav', 'rb'))
+loaded_vec= pickle.load(open('vec_model.sav', 'rb'))
+
 
 # membaca data berlabel hasil lexicon dan naive bayes
 data = pd.read_csv('tweets_all_data_clean.csv')
@@ -14,4 +20,18 @@ data_show= pd.DataFrame(data={'negatif' : [neg], 'positif': [pos], 'netral':[net
 st.title('Selamat datang di aplikasi Senasi')
 # menampilkan teks sub judul dan bar chart
 st.subheader('Hasil Sentimen Inflasi dari komentar twitter')
+# Preformatted text
+st.text("This is fixed-width text")
 st.bar_chart(data_show.T) 
+
+
+# Membuat prediksi
+st.subheader('Prediksi hasil sentimen inflasi')
+
+user_input = st.text_input("tweets")
+
+res = loaded_model.predict(loaded_vec.transform(['coba ngobrolin geopolitik eropa timur dan inflasi turki yg meroket']))
+
+if st.button('Prediksi'):
+    st.write('Hasil Sentimen : ')
+    st.write(res[0])
